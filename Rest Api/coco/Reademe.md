3efed08f0f2ad4885ce1d31473f851ccf4c896d0
AakashJyoti <akjyoti572@gmail.com>
2022-10-06 00:01:19 +0530

    coco restart

---
+++ Rest Api/coco/Reademe.md
@@ -0,0 +1,11 @@
+List of Tasks:-
+
+- Register a user
+- Login a user
+- Who am I
+- Add new product
+- Update a product
+- Get all product
+- Get single product
+- Delete a product
+
--- Rest Api/coco/routes/index.js
+++
@@ -1,8 +0,0 @@
-import Express from "express";
-import { registerController } from "../controllers";
-
-const router = Express.Router();
-
-router.post("/register", registerController.register);
-
-export default router;
