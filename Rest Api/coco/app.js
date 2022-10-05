51deef1d8ea6ba794d3e68704925374e6105c761
AakashJyoti <akjyoti572@gmail.com>
2022-10-06 00:26:22 +0530

    minnor changes

--- Rest Api/coco/app.js
+++ Rest Api/coco/app.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { APP_PORT } from "./config";
+import routes from "./routes";
 
 const app = express();
 
-
+app.use(routes);
 
 app.listen(APP_PORT, () =>
   console.log(`server running on http://localhost:${APP_PORT}`)
