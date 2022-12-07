110cb622a2445ce635a8bdf2e278c3ecfa5728d0
AakashJyoti <akjyoti572@gmail.com>
2022-12-08 00:09:14 +0530

    TS NotePad

---
+++ typeScript/notesPad/src/NewNote.tsx
@@ -0,0 +1,11 @@
+import NewForm from "./components/NewForm";
+
+const NewNote = () => {
+  return (
+    <>
+      <h1 className="md-4">NewNote</h1>
+      <NewForm />
+    </>
+  );
+};
+export default NewNote;
