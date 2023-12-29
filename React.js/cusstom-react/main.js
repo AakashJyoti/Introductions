505112e230ff050326186244b7be08163e46c102
AakashJyoti <akjyoti572@gmail.com>
2023-12-29 14:40:42 +0530

    custom react update

--- React.js/cusstom-react/main.js
+++ React.js/cusstom-react/main.js
@@ -1,20 +1,44 @@
 const mainContainer = document.getElementById("root");
 
 const reactElement = {
-  type: "a",
+  type: "div",
   props: {
-    href: "https://google.com",
-    target: "_blank",
+    className: "style",
+    style: (style = "display: flex; flex-direction: column"),
   },
-  children: "Click me to visit Google ",
+  children: [
+    {
+      type: "a",
+      props: {
+        href: "https://google.com",
+        target: "_blank",
+      },
+      value: "Click me to visit Google",
+      children: [],
+    },
+    {
+      type: "a",
+      props: {
+        href: "https://github.com",
+        target: "_blank",
+      },
+      value: "Click me to visit GitHub",
+      children: [],
+    },
+  ],
 };
 
 function handelRender(comp, main) {
   const domElement = document.createElement(comp.type);
-  domElement.innerHTML = comp.children;
   for (const prop in comp.props) {
     if (prop === "children") continue;
     domElement.setAttribute(prop, comp.props[prop]);
+  }
+  if (comp.value) domElement.innerHTML = comp.value;
+  if (comp.children.length) {
+    for (const childElement of comp.children) {
+      handelRender(childElement, domElement);
+    }
   }
   main.appendChild(domElement);
 }
