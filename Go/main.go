3bf7d7b54174d188f21df87a0480c2ab28cd6900
AakashJyoti <akjyoti572@gmail.com>
2024-07-12 23:18:27 +0530

    go init add

---
+++ Go/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello mom")
+
+	// Simple values
+
+	// int
+	fmt.Println(1 + 1)
+	// string
+	fmt.Println("Hello there")
+	// bool
+	fmt.Println(true)
+	// floats
+	fmt.Println(10.5)
+
+	// Variables
+
+	// var name string = "Astr"
+	// var name = "Astr"
+	name := "Astr"
+
+	fmt.Println(name)
+
+}
