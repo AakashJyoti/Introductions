72d1bcb42e2aed883fbf688aeee6cdf399d17c96
AakashJyoti <akjyoti572@gmail.com>
2024-07-14 19:48:25 +0530

    go updated

--- Go/main.go
+++ Go/main.go
@@ -5,35 +5,63 @@ import "fmt"
 const age = 57
 
 func main() {
-	fmt.Println("Hello mom")
+	// fmt.Println("Hello mom")
 
 	// Simple values
 
 	// int
-	fmt.Println(1 + 1)
+	// fmt.Println(1 + 1)
+
 	// string
-	fmt.Println("Hello there")
+	// fmt.Println("Hello there")
+
 	// bool
-	fmt.Println(true)
+	// fmt.Println(true)
+
 	// floats
-	fmt.Println(10.5)
+	// fmt.Println(10.5)
 
 	// Variables
 
 	// var name string = "Astr"
 	// var name = "Astr"
-	name := "Astr"
+	// name := "Astr"
 
-	fmt.Println(name)
+	// fmt.Println(name)
 
 	// age = 14 // not allowed
-	fmt.Println(age)
+	// fmt.Println(age)
 
-	const (
-		port = 5000
-		host = "localhost"
-	)
+	// const (
+	// 	port = 5000
+	// 	host = "localhost"
+	// )
 
-	fmt.Println(host)
-	fmt.Println(port)
+	// fmt.Println(host)
+	// fmt.Println(port)
+
+	// for -> only constructor for looping
+
+	// while loop
+	// i := 1
+	// for i <= 3 {
+	//	fmt.Println(i)
+	// 	i = i + 1
+	// }
+
+	// infinite loop
+	// for {
+	// }
+
+	// classic for loop
+	// for i := 0; i <= 3; i++ {
+	// 	break
+	// 	continue
+	// 	fmt.Println(i)
+	// }
+
+	for i := range 677 {
+		fmt.Println(i)
+	}
+
 }
